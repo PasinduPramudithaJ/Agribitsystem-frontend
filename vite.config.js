e272c491f1a373dabc4252ee2f460ce5a2a8386e
PasinduPramudithaJ <pramudithapasindu48@gmail.com>
2025-03-08 10:42:29 +0530

    Edit viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        
+        sassOptions: {
+          quietDeps: true, // This silences warnings from dependencies, including the legacy JS API warning
+          // silenceDeprecations: ['legacy-js-api'], // Not needed here as `quietDeps` should handle it
+        },
+      },
+    },
+  },
 })
